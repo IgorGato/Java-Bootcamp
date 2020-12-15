9ea20dc0ebb3faaca1367d6ba0cde411a7e09011
igor_gato <igormqg@gmail.com>
2020-12-15 00:25:46 -0300

    primeiros exercicios sobre orientacao a objetos

--- java/src/java_POO_lista_1/Eletro_obj.java
+++ java/src/java_POO_lista_1/Eletro_obj.java
@@ -1,5 +1,35 @@
 package java_POO_lista_1;
 
 public class Eletro_obj {
-
+	public int bateria = 100;
+	public String aplicativo = "Tela inicial";
+	
+	
+	
+	public void descarregar(int porcentagem) {
+		this.bateria -= porcentagem;
+	}
+	
+	public void aplicativos(char app) {
+		System.out.print("Voce esta usando ");
+		if(this.bateria <= 0) {
+			System.out.println("Voce esta sem bateria no momento");
+		}
+		if(app == 'W') {
+			System.out.println("Whatsapp");
+			descarregar(10);
+		}else if(app == 'F') {
+			System.out.println("Facebook");
+			descarregar(15);	
+		}else if(app == 'C') {
+			System.out.println("Candy Crush");
+			descarregar(20);
+		}
+	}
+	
+	public void porcentagem() {
+		if(this.bateria < 0)
+			this.bateria = 0;
+		System.out.printf("voce tem %d %% de bateria\n", this.bateria);
+	}
 }
--- java/src/java_POO_lista_1/eletronico.java
+++ java/src/java_POO_lista_1/eletronico.java
@@ -1,10 +1,27 @@
 package java_POO_lista_1;
 
+import java.util.Scanner;
+
 public class eletronico {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanf = new Scanner(System.in);
+		Eletro_obj celular = new Eletro_obj();
+		char opcao;
+		
+		
+		do{
+		System.out.println("Qual aplicacao voce gostaria de usar?");
+		System.out.println("Facebook, Whatsapp ou Candy Crush ou Porcentagem Bateria [F/W/C/B]");
+		opcao = scanf.next().toUpperCase().charAt(0);
+		if(opcao == 'B')
+			celular.porcentagem();
+		else 
+			celular.aplicativos(opcao);
+		
+		System.out.println("Deseja continuar? [S/N]");
+		opcao = scanf.next().toUpperCase().charAt(0);
+		}while(opcao == 'S');
 	}
 
 }
